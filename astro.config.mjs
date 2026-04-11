85593fab8a33f00b0643ddbe23ceb56b739b2236
FLC <pentalimbed@gmail.com>
2026-04-11 22:29:18 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  i18n: {
+    locales: ["en", "zh-cn"],
+    defaultLocale: "en",
+  },
+
+  integrations: [mdx()]
+});
