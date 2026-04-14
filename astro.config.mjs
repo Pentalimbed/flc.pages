17fb4e9d766ebdc66d6dbea9f9d2806dcaec8e61
FLC <pentalimbed@gmail.com>
2026-04-15 00:53:33 +0800

    self host fonts and css extraction

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,5 +19,72 @@ export default defineConfig({
   },
 
   integrations: [mdx()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
+
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "SUSE Mono",
+    cssVariable: "--font-suse-mono",
+    fallbacks: [],
+    optimizedFallbacks: false,
+    subsets: ["latin-ext"],
+    options: {
+      variants: [
+        {
+          weight: "100 800",
+          style: "normal",
+          src: ["./src/assets/fonts/SUSEMono[wght].woff2"],
+        },
+        {
+          weight: "100 800",
+          style: "italic",
+          src: ["./src/assets/fonts/SUSEMono-Italic[wght].woff2"],
+        },
+      ],
+    },
+  }, {
+    provider: fontProviders.local(),
+    name: "Newsreader",
+    cssVariable: "--font-newsreader",
+    fallbacks: [],
+    optimizedFallbacks: false,
+    subsets: ["latin-ext"],
+    options: {
+      variants: [
+        {
+          weight: "200 800",
+          style: "normal",
+          src: ["./src/assets/fonts/Newsreader[opsz,wght].woff2"],
+        },
+        {
+          weight: "200 800",
+          style: "italic",
+          src: ["./src/assets/fonts/Newsreader-Italic[opsz,wght].woff2"],
+        },
+      ],
+    },
+  }, {
+    provider: fontProviders.local(),
+    name: "Playfair",
+    cssVariable: "--font-playfair",
+    fallbacks: [],
+    optimizedFallbacks: false,
+    subsets: ["latin-ext"],
+    options: {
+      variants: [
+        {
+          weight: "300 900",
+          stretch: "87.5% 112.5%",
+          style: "normal",
+          src: ["./src/assets/fonts/PlayfairRomanVF.woff2"],
+        },
+        {
+          weight: "300 900",
+          stretch: "87.5% 112.5%",
+          style: "italic",
+          src: ["./src/assets/fonts/PlayfairItalicVF.woff2"],
+        },
+      ],
+    },
+  }]
 });
